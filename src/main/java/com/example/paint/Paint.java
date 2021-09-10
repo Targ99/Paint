0705ee6b867a182a30cd825fcca5020d7f247625
Targ99 <garrett.scheiber@valpo.edu>
2021-09-10 01:27:36 -0700

    Image resized correctly now

--- src/main/java/com/example/paint/Paint.java
+++ src/main/java/com/example/paint/Paint.java
@@ -1,7 +1,6 @@
 package com.example.paint;
-//JPG PNG
 
-import javafx.application.Application;
+import javafx.application.Application; //Imports
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -20,21 +19,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 
-public class Paint extends Application {
-    private int width = 700;
-    private int height = 700;
-    private Text file = new Text(" ");
-    private Text fileextension = new Text(" ");
-    VBox root = new VBox();
+public class Paint extends Application
+{
+    private float width = 700; //Class vars for height + width
+    private float height = 700;
+//    private Text file = new Text(" ");
+    private Text fileextension = new Text(" "); //Text for exception handling
+    private VBox root = new VBox(); //Vbox addressable by multiple functions
     @Override
     public void start(Stage window)
     {
-        //GridPane gridPane = new GridPane();
-        //Scene scene = new Scene();
-        //window.setFullScreen(true);
-        window.setHeight(height);
+        window.setHeight(height); //Setting stage height + width
         window.setWidth(width);
-        Menu menuFile = new Menu("File");
+        Menu menuFile = new Menu("File"); //instantiating the new menus
         Menu menuEdit = new Menu("Edit");
         MenuItem open = new MenuItem("Open");
         open.setOnAction(new EventHandler<ActionEvent>() {
@@ -51,51 +48,48 @@ public class Paint extends Application {
         window.setScene(Defaultscene);
         window.show(); //Constructs the stage
     }
-    private void findimg(Stage stage) //Opens a file explorer to open an image
+
+    private void findimg(Stage stage) //Opens a file explorer to find an image
     {
         FileChooser openfile = new FileChooser(); //Creates instance of file explorer
         openfile.setTitle("Open Image");
         File filename = openfile.showOpenDialog(stage); //Records the file to be opened
         openimg(filename); //Calling openimg() to open the file
     }
+
     private void openimg(File filename) //Opens file found in findimg()
     {
-        try{
+        try{ //Checking if the image is an accepted format
             String extension = Files.probeContentType(filename.toPath());
             if(extension.equals("image/jpeg") || extension.equals("image/png"))
             {
                 InputStream stream = new FileInputStream(filename);
-                Image image = new Image(stream);
-                //Creating the image view
+                Image image = new Image(stream); //instantiating image from file name
                 ImageView imageView = new ImageView();
-                //Setting image to the image view
                 imageView.setImage(image);
-                //Setting the image view parameters
-                imageView.setX(10);
-                imageView.setY(10);
-                if(image.getWidth() > image.getHeight())
+                if(image.getWidth() > image.getHeight()) //Attempt to fix oversized image
                 {
-                    imageView.setFitWidth(width);
+                    imageView.setFitWidth(width*0.75);
                 }
                 else
                 {
-                    imageView.setFitHeight(height);
+                    imageView.setFitHeight(height*0.75);
                 }
-                imageView.setPreserveRatio(true);
-                root.getChildren().add(imageView);
-//                System.out.print(image.getWidth());
+                imageView.setPreserveRatio(true); //Preserve the original aspect ratio
+                root.getChildren().add(imageView); //Adding image to Vbox to display
+//                System.out.print(image.getWidth()); //Attempt to fix oversized image
 //                System.out.print(image.getHeight());
             }
             else
             {
-                fileextension.setText("File Type Not Supported");
+                fileextension.setText("File Type Not Supported"); //Case if unaccepted format
 //                fileextension.setText(extension);
-                System.out.print(extension);
+//                System.out.print(extension);
             }
         }
         catch (IOException e)
         {
-            fileextension.setText("File Extension not Recognized");
+            fileextension.setText("File Extension not Recognized"); //Case if format checking fails
         }
     }
 
