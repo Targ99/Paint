804dc91b0f2479d88fcd5f7edb6caa9f99e0b246
Targ99 <garrett.scheiber@valpo.edu>
2021-09-09 18:44:44 -0700

    Beginning Stages

---
+++ src/main/java/com/example/paint/MenuBar.java
@@ -0,0 +1,19 @@
+package com.example.paint;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class MenuBar extends Application {
+    @Override
+
+}
